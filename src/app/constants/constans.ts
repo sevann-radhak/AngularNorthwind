ebefd2bd239dcdb3579ee4c3b4e3fc6c72421c25
Sevann Radhak <sevann.radhak@inworx.com>
2020-05-24 11:59:55 -0300

    improve employee detail

--- src/app/constants/constans.ts
+++ src/app/constants/constans.ts
@@ -2,6 +2,18 @@ export class Constants{
 
     public static ACCEPT = 'Accept';
     public static CANCEL = 'Cancel';
+
+    // Employee
+    public static ADDRESS = 'Address';
+    public static COMPANY = 'Company';
+    public static EMAIL = 'Email';
+    public static FIRST_NAME = 'First Name';
+    public static JOB_TITLE = 'Job Title';
+    public static LAST_NAME = 'Last Name';
+    public static PHONE_NUMBER = 'Phone Number';
+
+
+    // Product
     public static DELETE_PRODUCT_MESSAGE = 'Are you sure you want to delete this product?';
     public static DELETE_PRODUCT_TITLE = 'Delete Product';
     public static DISCARD_CHANGES = 'Do you want to discard the changes?';
--- src/app/employee/components/employee-detail/employee-detail.component.ts
+++ src/app/employee/components/employee-detail/employee-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Employee } from '../../models/employee';
+import { Constants } from './../../../constants/constans';
 
 @Component({
   selector: 'app-employee-detail',
@@ -7,9 +8,19 @@ import { Employee } from '../../models/employee';
   styleUrls: ['./employee-detail.component.scss']
 })
 export class EmployeeDetailComponent implements OnInit {
+  ADDRESS = Constants.ADDRESS;
+  COMPANY = Constants.COMPANY;
+  EMAIL = Constants.EMAIL;
+  FIRST_NAME = Constants.FIRST_NAME;
+  JOB_TITLE = Constants.JOB_TITLE;
+  LAST_NAME = Constants.LAST_NAME;
+  PHONE_NUMBER = Constants.PHONE_NUMBER;
 
   @Input()
   employee: Employee;
+
+  CONSTANTS: Constants;
+
   constructor() { }
 
   ngOnInit(): void {
